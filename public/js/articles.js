798b550514d645273bddc54a688e1476ad11d397
Lawrence Fiesta <lfiesta86@gmail.com>
2019-07-28 12:07:57 -0700

    after scraping it will reload the root screen and populate the data. didn't create a logic to prevent duplicate data entry

--- public/js/articles.js
+++ public/js/articles.js
@@ -4,14 +4,20 @@ $(document).ready(() => {
 
 function getArticles(){
   $.get("/api/articles", (req, res) => {
-    console.log(req, " what this be /public/js/articles.js");
+    // console.log(req.data.length, " what this be /public/js/articles.js");
 
 
-    for(var i = 0; i < req.lenght; i++){
+    for(var i = 0; i < req.data.length; i++){
+      console.log(req.data[i]._id, "for loop id");
       var divOne = $("<div>");
       divOne.attr({
         "data-_id": req.data[i]._id,
         class: "card"
+      });
+
+      var divTwo = $("<div>");
+      divTwo.attr({
+        class: "card-header"
       });
 
       var h3One = $("<h3>");
@@ -20,13 +26,29 @@ function getArticles(){
       aOne.attr({
         class: "article-link",
         target: "_blank",
-        rel= "noopener noreferrer",
-        href= "https://www.bbc.com/" + req.data[i].link,
-      })
+        rel: "noopener noreferrer",
+        href: "https://www.bbc.com/" + req.data[i].link,
+      });
+      aOne.text(req.data[i].title);
+
+      var aTwo = $("<a>");
+      aTwo.attr({
+        class: "btn btn-success save"
+      });
+      aTwo.text("Save Article");
+
+      var divThree = $("<div>");
+      divThree.addClass("card-body");
+      divThree.text(req.data[i].summary);
 
 
+      h3One.append(aOne);
+      h3One.append(aTwo);
+      divTwo.append(h3One);
 
-      divOne.append(h3One);
+      divOne.append(divTwo);
+      divOne.append(divThree);
+      $(".article-container").append(divOne);
     }
   })
 }
