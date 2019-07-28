8e44d413bcb5e89688da7b159c8b3cda9f6b5b0c
Lawrence Fiesta <lfiesta86@gmail.com>
2019-07-28 12:50:28 -0700

    trying to get heroku to deploy

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.set("view engine", "handlebars");
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
